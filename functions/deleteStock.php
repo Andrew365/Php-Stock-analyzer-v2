7c15f47ae4fc6d3d650802ed20c5f7cfd37c6250
AWboy <baseball_allstar_365@hotmail.com>
2015-07-27 14:16:05 -0600

    updated delete stock

--- functions/deleteStock.php
+++ functions/deleteStock.php
@@ -8,7 +8,9 @@ function deleteStock(){
     $ticker = $_POST['ticker'];
      $sql = "DROP TABLE {$ticker}";
      $data = mysqli_query($connect, $sql);
-
+     if(!data){
+       echo mysqli_error($connect);
+     }
      $sql2 = "DELETE FROM `analysis_a` WHERE ticker = '{$ticker}'";
      $data2 = mysqli_query($connect, $sql2);
 
